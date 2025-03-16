c3efe8cf5b6e4eccca5b7731b8bbfe1a9b1156c4
chuchito-sama <pt77564074@idat.pe>
2025-03-16 18:30:23 -0500

    EC3-ccanquibartolo

--- src/main/java/org/idat/pe/ec3ccanquibartolo/Model/Category.java
+++ src/main/java/org/idat/pe/ec3ccanquibartolo/Model/Category.java
@@ -1,59 +1,7 @@
 package org.idat.pe.ec3ccanquibartolo.Model;
 
 
-import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "categories")
 public class Category {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private String name;
-
-    private String description;
-
-    @Column(name = "created_at")
-    private LocalDateTime createdAT;
-
-    public Category() {
-        this.createdAT = LocalDateTime.now();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreatedAT() {
-        return createdAT;
-    }
-
-    public void setCreatedAT(LocalDateTime createdAT) {
-        this.createdAT = createdAT;
-    }
 }
